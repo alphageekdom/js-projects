644cdd9e1e86828d8947d863c1cea231d613b847
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2023-10-11 14:43:31 -0700

    details backdrop iamge

--- flixx-movie-app/js/script.js
+++ flixx-movie-app/js/script.js
@@ -64,6 +64,9 @@ async function displayMovieDetails() {
   const movie = await fetchAPIData(`movie/${movieId}`);
   const div = document.createElement('div');
 
+  //   Overlay Background Image
+  displayBackgroundImage('movie', movie.backdrop_path);
+
   div.innerHTML = `
   <div class="details-top">
           <div>
@@ -116,6 +119,28 @@ async function displayMovieDetails() {
   `;
 
   document.querySelector('#movie-details').appendChild(div);
+}
+
+// Display Backdrop Image
+function displayBackgroundImage(type, backgroundPath) {
+  const overlayDiv = document.createElement('div');
+  overlayDiv.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${backgroundPath})`;
+  overlayDiv.style.backgroundSize = 'cover';
+  overlayDiv.style.backgroundPosition = 'center';
+  overlayDiv.style.backgroundRepeat = 'no-repeat';
+  overlayDiv.style.height = '100vh';
+  overlayDiv.style.width = '100vw';
+  overlayDiv.style.position = 'absolute';
+  overlayDiv.style.top = '0';
+  overlayDiv.style.left = '0';
+  overlayDiv.style.zIndex = '-1';
+  overlayDiv.style.opacity = '0.1';
+
+  if (type === 'movie') {
+    document.querySelector('#movie-details').appendChild(overlayDiv);
+  } else {
+    document.querySelector('#show-details').appendChild(overlayDiv);
+  }
 }
 
 // Format Currency
