66f8d43d47bac459f564427e1838ae43a93959ea
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2023-10-14 13:33:03 -0700

    remove meals and workouts from local storage

--- tracalorie-app/js/app.js
+++ tracalorie-app/js/app.js
@@ -41,6 +41,7 @@ class CalorieTracker {
       this._totalCalories -= meal.calories;
       Storage.updateTotalCalories(this._totalCalories);
       this._meals.splice(index, 1);
+      Storage.removeMeals(id);
       this._render();
     }
   }
@@ -52,6 +53,7 @@ class CalorieTracker {
       this._totalCalories += workout.calories;
       Storage.updateTotalCalories(this._totalCalories);
       this._workouts.splice(index, 1);
+      Storage.removeWorkouts(id);
       this._render();
     }
   }
@@ -253,6 +255,17 @@ class Storage {
     localStorage.setItem('meals', JSON.stringify(meals));
   }
 
+  static removeMeals(id) {
+    const meals = Storage.getMeals();
+    meals.forEach((meal, index) => {
+      if (meal.id === id) {
+        meals.splice(index, 1);
+      }
+    });
+
+    localStorage.setItem('meals', JSON.stringify(meals));
+  }
+
   static getWorkouts() {
     let workouts;
     if (localStorage.getItem('workouts') === null) {
@@ -266,6 +279,16 @@ class Storage {
   static saveWorkouts(workout) {
     const workouts = Storage.getWorkouts();
     workouts.push(workout);
+    localStorage.setItem('workouts', JSON.stringify(workouts));
+  }
+  static removeWorkouts(id) {
+    const workouts = Storage.getWorkouts();
+    workouts.forEach((workout, index) => {
+      if (workout.id === id) {
+        workouts.splice(index, 1);
+      }
+    });
+
     localStorage.setItem('workouts', JSON.stringify(workouts));
   }
 }
