24bb9c06c0cd53e0e56681c70926ab3152675108
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2023-10-18 14:28:16 -0700

    API Service - get ideas

--- randomideas-api/server.js
+++ randomideas-api/server.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
+const cors = require('cors');
 require('dotenv').config();
-
 const port = process.env.PORT || 5000;
 const connectDB = require('./config/db');
 
@@ -15,6 +15,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// Cors middleware
+app.use(
+  cors({
+    origin: ['http://localhost:5000', 'http://localhost:3000'],
+    credentials: true,
+  })
+);
 
 // Welcome Route
 app.get('/', (req, res) => {
